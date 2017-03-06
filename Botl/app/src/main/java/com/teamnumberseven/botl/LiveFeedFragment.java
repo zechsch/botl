522e6cb423776aee20170852d5b561604a9e53bd
kkuang70 <veinkuang@gmail.com>
2017-03-06 14:10:00 -0500

    Split View

---
+++ Botl/app/src/main/java/com/teamnumberseven/botl/LiveFeedFragment.java
@@ -0,0 +1,20 @@
+package com.teamnumberseven.botl;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Kevin on 3/2/2017.
+ */
+
+public class LiveFeedFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.feed_fragment, container, false);
+        return view;
+    }
+}
---
+++ Botl/app/src/main/java/com/teamnumberseven/botl/MainActivity.java
@@ -0,0 +1,29 @@
+package com.teamnumberseven.botl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        SupportMapFragment mapFrag =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFrag.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(0,0)).title("Marker"));
+    }
+}
